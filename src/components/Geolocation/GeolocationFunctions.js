00a082b18ec7692b91910bcabb36e3463a8b3293
Yunus Emre Sirin <yunus.sirin@smail.inf.h-brs.de>
2024-09-27 16:08:30 +0200

    .

--- src/components/Geolocation/GeolocationFunctions.js
+++ src/components/Geolocation/GeolocationFunctions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useSession } from '../SessionProvider';
 import {
     ListItem,
